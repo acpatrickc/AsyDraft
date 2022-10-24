d030ee1ebf4838fbef02c1bcdc8d58510e2073c8
Patrick Cao <acpatrickc@gmail.com>
2022-10-24 00:27:11 -0400

    Initialization class annotated and renamed

--- src/main/java/AsyDraft/ui/Init.java
+++ src/main/java/AsyDraft/ui/Init.java
@@ -2,20 +2,21 @@ package AsyDraft.ui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class App extends Application {
-
+public class Init extends Application {
+	/*
+	 * starts application and creates a scene with a MainPane
+	 */
     @Override
     public void start(Stage stage) {
-       
     	Scene scene = new Scene(new MainPane(), 1000, 600);
         stage.setScene(scene);
         stage.show();
     }
-
+    /*
+     * main method
+     */
     public static void main(String[] args) {
         launch();
     }
